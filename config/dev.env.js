57e1cc175693865c70979abb828f8f2b2b08bd8b
Kirk Chu <kkchu791@gmail.com>
2025-05-23 11:04:54 -0700

    add ion token in .env

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,6 @@ const { merge } = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  ION_ACCESS_TOKEN: '"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjY2U0YmYyZS04NTRjLTQ1N2UtYTEyMS0wMTUwMDllOTA1YWIiLCJpZCI6MzA1MzgzLCJpYXQiOjE3NDc5Mzg3MzR9.jRP1mJknonTz3Govl0ipJVxVClohP3Y0ZCxzfL7aZwo"'
 })
